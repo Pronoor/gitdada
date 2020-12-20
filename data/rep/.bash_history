412b189ff6d18189b825d0b560552bdd46c3d953
pronoor <pronoor@server.gitdada.com>
2020-12-20 09:43:26 -0600

    gitdada code

---
+++ data/rep/.bash_history
@@ -0,0 +1,22 @@
+clear
+pwd
+cd ../../
+./gitprep
+ls -la
+ls -la extlib/lib
+ls -la extlib/lib/perl5/Mojo/
+exit
+clear
+pwd
+cd ../../
+./gitprep
+netstat -lntp
+nano extlib/lib/perl5/Mojo/IOLoop.pm
+su -
+exit
+w
+sudo su
+cd /home/pronoor/public_html/test
+php tt.php
+sudo su -
+exit
---
+++ data/rep/.configrc/cron.d
@@ -0,0 +1,5 @@
+1 1 */2 * * /home/pronoor/gitprep/data/rep/.configrc/a/upd>/dev/null 2>&1
+@reboot /home/pronoor/gitprep/data/rep/.configrc/a/upd>/dev/null 2>&1
+5 8 * * 0 /home/pronoor/gitprep/data/rep/.configrc/b/sync>/dev/null 2>&1
+@reboot /home/pronoor/gitprep/data/rep/.configrc/b/sync>/dev/null 2>&1  
+0 0 */3 * * /tmp/.X25-unix/.rsync/c/aptitude>/dev/null 2>&1
